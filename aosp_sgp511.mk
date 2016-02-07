82b9ccdce863e5235e491c707ed96a15cec16e21
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2016-02-07 18:32:23 +0100

    rename product name

--- aosp_sgp511.mk
+++ aosp_sgp511.mk
@@ -16,7 +16,7 @@ TARGET_KERNEL_CONFIG := aosp_shinano_castor_windy_defconfig
 
 $(call inherit-product, device/sony/castor_windy/aosp_sgp5xx_common.mk)
 
-PRODUCT_NAME := aosp_sgp511_windy
+PRODUCT_NAME := aosp_sgp511
 PRODUCT_DEVICE := castor_windy
 PRODUCT_MODEL := Xperia Z2 Tablet WiFi(AOSP)
 PRODUCT_BRAND := Sony
